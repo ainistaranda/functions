c0bdaa5518ac6b888a83f8c715c81e9a536c32e8
Ainis Taranda <taranda.ainis@gmail.com>
2022-10-06 14:38:05 -0400

    added minus

--- function.js
+++ function.js
@@ -3,8 +3,13 @@
 // const addTwo = (a, b) => {
 //   return a + b
 // }
+
 const addTwo = require("./external.js")
+const subtract = require('./minus') 
 
+// const sum = addTwo(3, 4)
+// console.log(sum)
 
-const sum = addTwo(3, 4)
-console.log(sum)
+const total = subtract(10,5)
+console.log(total)
+
